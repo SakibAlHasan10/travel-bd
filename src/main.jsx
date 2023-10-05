f3f7028eafd686e872f0789d8f11ce4c40fca05c
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-05 19:14:43 +0600

    add home, root, router, auth provider pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import AuthProvider from './AuthProvider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
+      
+    </AuthProvider>
   </React.StrictMode>,
 )
